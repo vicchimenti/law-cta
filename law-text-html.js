0ce9648ae208096d59ac187f90d46959662a916c
vicchimenti <victor.chimenti@gmail.com>
2021-04-14 13:56:22 -0700

    rm marg

--- law-text-html.js
+++ law-text-html.js
@@ -54,7 +54,7 @@
     var closeOverlay = '</div>';
     var opneOverlayStyle = '<div class="callToActionOverlay callToActionOverlayColor' + colorOverlay + ' m-0">';
     var closeOverlayStyle = '</div>';
-    var openBlock = '<div class="card-block p-1 m-1">';
+    var openBlock = '<div class="card-block p-1">';
     var closeBlock = '</div>';
     var openCallToAction = '<div class="callToAction">';
     var closeCallToAction = '</div>';
