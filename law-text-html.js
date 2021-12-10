3644b34ad759decc8382350ec1ce6b87d1109e61
vicchimenti <victor.chimenti@gmail.com>
2021-12-10 11:10:43 -0800

    color overlay

--- law-text-html.js
+++ law-text-html.js
@@ -109,6 +109,17 @@
     let ctafDict = {
 
         contentName: getContentValues('<t4 type="content" name="Name" output="normal" modifiers="striptags,htmlentities" />'),
+        headline: getContentValues('<t4 type="content" name="Headline" output="normal" modifiers="striptags,htmlentities" />'),
+        headlineColor: getContentValues('<t4 type="content" name="Headline color" output="normal" display_field="value" />'),
+        backgroundImage: getContentValues('<t4 type="content" name="Background Image" output="normal" formatter="path/*" />'),
+        colorOverlay: getContentValues('<t4 type="content" name="Color Overlay" output="normal" display_field="value" />'),
+
+
+
+
+
+        anchorTag: getContentValues('<t4 type="meta" meta="html_anchor" />'),
+        contentId: getContentValues('<t4 type="meta" meta="content_id" />')
 
     }
 
@@ -137,7 +148,7 @@
     var btnFiveLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Link #5' output='linkurl' modifiers='nav_sections' />");
     var btnFiveTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Link #5' output='linktext' modifiers='nav_sections' />");
     var anchorTag = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='html_anchor' />");
-    var contentID = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
+    var contentId = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
 
 
 
@@ -155,12 +166,12 @@
     var closeBlock = '</div>';
     var openCardTitle = '<div class="card-title p-3">';
     var closeCardTitle = '</div>';
-    var headlineString = '<h2 class="callToActionHeaderColor' + headlineColor + ' my-2" id="label' + contentID + '">' + headline + '</h2>';
+    var headlineString = '<h2 class="callToActionHeaderColor' + headlineColor + ' my-2" id="label' + contentId + '">' + headline + '</h2>';
     var openLinksList = '<ul class="callToActionButtonWrapper standardContent d-flex flex-col flex-md-row flex-md-nowrap justify-md-content-between">';
     var closeLinksList = '</ul>';
     var btnOneString = '<li class="callToActionButton"><a href="' + btnOneLink + '" title="' + btnOneTitle + '">' + btnOneText + '</a></li>';
     var buttonListString = '' + btnOneString + '';
-    var beginningHTML = '<div class="callToActionWrapper contentItem container-fluid g-0" title="' + headline + '" id="id' + contentID + '" data-position-default="Main" data-position-selected="Main" role="presentation" ><div class="col-12 card border-0">';
+    var beginningHTML = '<div class="callToActionWrapper contentItem container-fluid g-0" title="' + headline + '" id="id' + contentId + '" data-position-default="Main" data-position-selected="Main" role="presentation" ><div class="col-12 card border-0">';
     var endingHTML = '</div></div>';
 
 
